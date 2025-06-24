dbc4a8501124ef438055255eb4c4580cfb0f95c7
Skybit100 <skybit@icloud.com>
2025-06-24 17:38:53 -0600

    Fix smooth scrolling script to handle missing targets

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
   });
 });
