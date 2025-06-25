c2c3809bfe6b5bf5682c85c3e4d5481f9056876e
JuanJo <juanjconde@gmail.com>
2025-06-25 17:24:35 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('[data-section]').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const sectionId = link.getAttribute('data-section');
-    document.querySelectorAll('.seccion').forEach(sec => sec.classList.remove('visible'));
-    document.getElementById(sectionId).classList.add('visible');
-  });
-});
